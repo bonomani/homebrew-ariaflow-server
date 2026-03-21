99586ce5c7c2d508106b7d86e38fa215a39ab527
Bonomani <8841264+bonomani@users.noreply.github.com>
2026-03-21 22:38:54 +0100

    Add ariaflow-web formula

---
+++ Formula/ariaflow-web.rb
@@ -0,0 +1,32 @@
+class AriaflowWeb < Formula
+  desc "Local dashboard frontend for ariaflow"
+  homepage "https://github.com/bonomani/ariaflow-web"
+  url "https://github.com/bonomani/ariaflow-web/archive/refs/heads/main.tar.gz"
+  sha256 "9f08c5efe9b96dddb6722e45174e8b5654792931fd48680330bfec4ba7560a3e"
+  version "0.1.0"
+  license "MIT"
+  depends_on "python"
+
+  def install
+    libexec.install "src"
+
+    (bin/"ariaflow-web").write <<~EOS
+      #!/bin/bash
+      exec env PYTHONPATH="#{libexec}/src:${PYTHONPATH}" python3 -m ariaflow_web.cli "$@"
+    EOS
+    chmod 0755, bin/"ariaflow-web"
+  end
+
+  service do
+    environment_variables ARIAFLOW_API_URL: "http://127.0.0.1:8000"
+    run [opt_bin/"ariaflow-web", "--host", "127.0.0.1", "--port", "8001"]
+    keep_alive true
+    working_dir var
+    log_path var/"log/ariaflow-web.log"
+    error_log_path var/"log/ariaflow-web.err.log"
+  end
+
+  test do
+    system bin/"ariaflow-web", "--version"
+  end
+end
