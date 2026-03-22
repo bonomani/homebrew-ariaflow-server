2eee802a50f0898e41837629b180436fa0dabc62
Bonomani <8841264+bonomani@users.noreply.github.com>
2026-03-22 06:29:29 +0100

    Bump ariaflow to alpha 42

--- Formula/ariaflow.rb
+++ Formula/ariaflow.rb
@@ -1,9 +1,9 @@
 class Ariaflow < Formula
   desc "Sequential aria2 queue driver with adaptive bandwidth control"
   homepage "https://github.com/bonomani/ariaflow"
-  url "https://github.com/bonomani/ariaflow/archive/refs/tags/v0.1.1-alpha.41.tar.gz"
-  sha256 "6cc89b71a96bf4ef285524a7ac0c86b74afae5b13ef9dbbb44955aa66d4453b5"
-  version "0.1.1-alpha.41"
+  url "https://github.com/bonomani/ariaflow/archive/refs/tags/v0.1.1-alpha.42.tar.gz"
+  sha256 "331638374c93cc3c59afb44b6a21e5c8b4e8f3fc83441bb73650aaad8e326232"
+  version "0.1.1-alpha.42"
   license "MIT"
   depends_on "python"
   depends_on "aria2"
